c8b862ff4fbcb705dc62d0110df07ef8697c4400
corplucascosta-crypto <corp.lucascosta@gmail.com>
2026-08-03 14:56:56 -0300

    Recriando pastas do zero com nomes em minúsculo para o GitHub Pages

--- utils/helpers.js
+++
@@ -1,6 +0,0 @@
-export const formatCurrency = (value) => {
-    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value);
-};
-export const formatNumber = (value) => {
-    return new Intl.NumberFormat('pt-BR').format(value);
-};
