82451214809bc2808bbd75bbc6d22b39f3000bc4
corplucascosta-crypto <corp.lucascosta@gmail.com>
2026-08-03 15:21:28 -0300

    Correção definitiva: recriando pastas minúsculas forçadamente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,2 @@
-console.log("G&A Kids ERP carregado!");
-document.getElementById('sidebar').innerHTML = '<div class="brand"><div class="logo">G&A</div><span class="name">G&A Kids</span></div><nav class="nav"><a href="/dashboard/" class="nav-item active"><i class="fas fa-chart-pie"></i> Dashboard</a></nav>';
+console.log("G&A Kids carregado!");
+document.body.style.backgroundColor = "#f0f0f0";
